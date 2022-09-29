6e004b923661216af6564b7fdd83dab42bdd9e4d
Serge1167 <99770416+Serge1167@users.noreply.github.com>
2022-09-29 10:57:25 -0400

    AddDestinationActivity

---
+++ workspace/ItinerairePlusPLus/app/src/main/java/ca/bntec/itineraireplusplus/AddDestinationActivity.kt
@@ -0,0 +1,11 @@
+package ca.bntec.itineraireplusplus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddDestinationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_destination)
+    }
+}
